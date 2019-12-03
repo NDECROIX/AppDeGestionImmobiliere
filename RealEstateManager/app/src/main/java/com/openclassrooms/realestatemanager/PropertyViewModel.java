42e914fe55499815fcf80d9393313cd9a8966d98
NDECROIX <nicolas.decroixx@gmail.com>
2019-12-03 16:17:41 +0100

    PropertyViewModel : Create ViewModel to manage properties

---
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/PropertyViewModel.java
@@ -0,0 +1,69 @@
+package com.openclassrooms.realestatemanager;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.openclassrooms.realestatemanager.model.Property;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+
+public class PropertyViewModel extends ViewModel {
+
+    // REPOSITORY
+    private final PropertyDataRepository propertyDataRepository;
+    private final Executor executor;
+
+    // CURRENT PROPERTY
+    private Property currentProperty;
+
+    public PropertyViewModel(PropertyDataRepository propertyDataRepository, Executor executor) {
+        this.propertyDataRepository = propertyDataRepository;
+        this.executor = executor;
+    }
+
+    /**
+     * Update the current property selected
+     *
+     * @param property Current property
+     */
+    public void setCurrentProperty(Property property) {
+        this.currentProperty = property;
+    }
+
+    /**
+     * Get the last selected property
+     *
+     * @return Last selected property
+     */
+    public Property getCurrentProperty() {
+        return currentProperty;
+    }
+
+    /**
+     * Get all property from the application database
+     *
+     * @return List of all property
+     */
+    public LiveData<List<Property>> getPropertys() {
+        return propertyDataRepository.getPropertys();
+    }
+
+    /**
+     * Insert a property in the database
+     *
+     * @param property New property
+     */
+    public void insertProperty(Property property) {
+        executor.execute(() -> propertyDataRepository.insertProperty(property));
+    }
+
+    /**
+     * Update a property from the application database
+     *
+     * @param property Property to update
+     */
+    public void updateProperty(Property property) {
+        executor.execute(() -> propertyDataRepository.updateProperty(property));
+    }
+}
