52d7b602f06c352917638e60e475177e3bc631a4
NDECROIX <nicolas.decroixx@gmail.com>
2019-12-08 14:21:59 +0100

    EditActivity : Fill in the EditActivity layout

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/EditActivity.java
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/controllers/activities/EditActivity.java
@@ -2,11 +2,21 @@ package com.openclassrooms.realestatemanager.controllers.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.openclassrooms.realestatemanager.R;
 
 public class EditActivity extends AppCompatActivity {
+
+    public interface startEditActivityListener{
+        void createProperty();
+    }
+
+    public static Intent newIntent(Context context){
+        return new Intent(context, EditActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
